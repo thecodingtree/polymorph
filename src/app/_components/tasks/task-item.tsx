8c769025c63ca54e6b6e2df4a32c512ae049c10c
David Mitchell <dmitchek@gmail.com>
2025-01-23 21:04:33 -0500

    Can now delete tasks
    This is very much an initial pass as there's nothing preventing accidental deletions and once they are gone, they are gone. But this will be helpful in the development process to be able to cleanup test task

--- src/app/_components/tasks/task-item.tsx
+++ src/app/_components/tasks/task-item.tsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
+import { CircleX } from "lucide-react";
+
 import { Checkbox } from "~/app/_components/ui/checkbox";
+import { Button } from "~/app/_components/ui/button";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "~/app/_components/ui/collapsible";
 
 import { cn } from "~/lib/utils";
 
@@ -7,25 +18,56 @@ import type { Task } from "~/types";
 export default function TaskItem({
   task,
   onChecked,
+  onDelete,
 }: {
   task: Task;
   onChecked?: (checked: boolean) => void;
+  onDelete?: (taskId: string) => void;
 }) {
+  const [opened, setOpened] = useState(false);
+
   const isPending = task.id.includes("pending");
 
   return (
-    <div
-      className={cn(
-        "flex items-center space-x-2 p-2",
-        isPending && "opacity-50",
-      )}
-    >
-      <Checkbox
-        checked={task.completed}
-        onCheckedChange={onChecked}
-        disabled={isPending}
-      />
-      <span className="ml-2">{task?.title}</span>
-    </div>
+    <Collapsible className="w-full" onOpenChange={setOpened}>
+      <CollapsibleTrigger asChild className="w-full">
+        <div
+          className={cn(
+            "flex h-8 items-center space-x-2",
+            isPending ? "opacity-50" : "",
+          )}
+        >
+          <Checkbox
+            checked={task.completed}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            onCheckedChange={onChecked}
+            disabled={isPending}
+          />
+          <span
+            className={cn(
+              "ml-2 flex-1 text-left",
+              task?.completed ? "line-through" : "",
+            )}
+          >
+            {task?.title}
+          </span>
+          {opened && (
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={(e) => {
+                e.stopPropagation();
+                onDelete?.(task.id);
+              }}
+            >
+              <CircleX />
+            </Button>
+          )}
+        </div>
+      </CollapsibleTrigger>
+      <CollapsibleContent>Open</CollapsibleContent>
+    </Collapsible>
   );
 }
